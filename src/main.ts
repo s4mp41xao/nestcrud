aa7a9ef3017b04a63638000e7398fcad9fa5d47e
s4mp41xao <samuelpaixao@proton.me>
2025-08-18 23:55:02 -0300

    Enables CORS with origin validation
    Sets up Cross-Origin Resource Sharing (CORS) to allow requests from specific origins.
    
    It reads the allowed origins from the CORS_ORIGIN environment variable, and if the variable isn't set, it logs a warning. It validates the origin against the allowed origins specified in the environment and only allows requests from whitelisted origins.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const clientUrl = configService.get<string>('CORS_ORIGIN');
+  if (!clientUrl) {
+    Logger.warn(
+      'CORS_ORIGIN not found in .env file. Browser requests may be blocked.',
+      'Bootstrap',
+    );
+  }
+  const allowedOrigins = clientUrl ? clientUrl.split(',') : [];
+
+  Logger.log(`CORS whitelist: [${allowedOrigins.join(', ')}]`, 'Bootstrap');
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
